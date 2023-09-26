3c213226c373d6905723bb777e9192d78fa4df8e
Emil Pang <pang.216@buckeyemail.osu.edu>
2023-09-26 14:38:47 -0400

    Added file io, test folder, license

--- gruvarbetare.py
+++ gruvarbetare.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+#!/usr/bin/env python3
+
+import argparse
+from pathlib import Path
+
+def main():
+  parser = argparse.ArgumentParser(prog="gruvarbetare")
+  parser.add_argument("-f", "--file", dest="filename", help="file to unpack", metavar="FILE")
+  args = parser.parse_args()
+
+  if args.filename:
+    try:
+      f = open(args.filename, 'r')
+    except FileNotFoundError:
+      print("Error: file does not exist.")
+
+if __name__ == '__main__':
+  main()
