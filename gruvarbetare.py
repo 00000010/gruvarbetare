8d680efb6d9f4fced81b3f87156c363cb159f347
Emil Pang <pang.216@buckeyemail.osu.edu>
2023-10-07 13:42:15 -0400

    Added magic, setup

--- gruvarbetare.py
+++ gruvarbetare.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
+# TODO: Research packed binaries: what they look like, 
 import argparse
 from pathlib import Path
+import magic
+
+def identify(f):
+#  while (byte := f.read(1)):
+#    print(byte)
+  return
 
 def main():
+  # Parse program args
   parser = argparse.ArgumentParser(prog="gruvarbetare")
   parser.add_argument("-f", "--file", dest="filename", help="file to unpack", metavar="FILE")
   args = parser.parse_args()
 
+  # Attempt to manipulate file
+  f = None
   if args.filename:
     try:
-      f = open(args.filename, 'r')
+      print(magic.from_file(args.filename))
+      print(magic.from_file(args.filename, mime = True))
+      with open(args.filename, 'rb') as f:
+        identify(f)
     except FileNotFoundError:
       print("Error: file does not exist.")
+      return
 
 if __name__ == '__main__':
   main()
---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='gruvarbetare',
+      version='1.0',
+      packages=['python-magic'],
+      )
