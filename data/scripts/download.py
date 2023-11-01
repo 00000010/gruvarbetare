0b1d7302232855d5a24fdcbbbd3aebb5eddeb30c
Emil Pang <pang.216@buckeyemail.osu.edu>
2023-11-01 04:11:40 +0000

    Forgot to add changes from previous commit; added changed files

--- data/scripts/download.py
+++ data/scripts/download.py
@@ -2,17 +2,22 @@ import pandas as pd
 import requests
 from os import listdir
 from os.path import isfile, join
-import process_module
+from process_module import process_module
 
 def get_hashes(processed_hashes_path):
   hashes = set()
-  with open(processed_hashes_path, 'r') as processed_hashes_file:
-    hashes.add(processed_hashes_file.readline())
+  try:
+    with open(processed_hashes_path, 'r') as processed_hashes_file:
+      for line in processed_hashes_file:
+        hashes.add(processed_hashes_file.readline().strip())
+  except FileNotFoundError:
+    return set()
   return hashes
 
 def main():
   print("Reading in hashes...")
   all_hashes = get_hashes('./all_hashes.txt')
+  print("hash count: " + str(len(all_hashes)))
   processed_hashes = get_hashes('./processed_hashes.txt')
   print("Done.")
   api_url = 'https://mb-api.abuse.ch/api/v1/'
@@ -23,23 +28,31 @@ def main():
   print("Processing 100 hashes...")
 
   # Open file for keeping track of processed hashes
-  with open('hashes.txt', 'a') as processed_hashes_file:
+  with open('processed_hashes.txt', 'a') as processed_hashes_file:
     # Go through each hash in sample list gathered from bazaar.abuse.ch
-    for h in hashes:
+    for h in all_hashes:
       # Do not process if sample already ingested
       if h in processed_hashes:
         continue
       
       # Download file according to hash to the downloaded folder
       params["sha256_hash"] = h
-      response = requests.post(api_url, data=params)
+      try:
+        response = requests.post(api_url, data=params)
+      except requests.ConnectionError:
+        print("Failed to download file. Are you connected to the Internet?")
+        return
       data = response.content
-      print(response.ok)
+      # TODO: Retry if response.ok not true
       with open('./downloaded/' + h + '.zip', 'wb') as s:
         s.write(data)
 
+      # Add to processed samples list
+      processed_hashes.add(h)
+      processed_hashes_file.write(h + "\n")
+
       # If 100 sample zip files have been downloaded, pause downloading to save on space and process them
-      if count > 100:
+      if count > 20:
         # Also ask user to disconnect from the internet, for safety while processing malware samples
         print("Pausing. Please disconnect from the internet to continue processing.")
         userInput = input("Type 'disconnected' once you have done so.\n")
@@ -50,12 +63,8 @@ def main():
 
         # Unzip and add sample data using process
         for zippedFile in [f for f in listdir(samplesPath) if isfile(join(samplesPath, f))]:
-          process_module(zippedFile, './sample_data.csv')
+          process_module(samplesPath + "/" + zippedFile, '../sample_data.csv')
           
-        # Add to processed samples list
-        processed_hashes.add(h)
-        processed_hashes_file.write(h + "\n")
-
         print("Done.")
 
         # Continue downloading samples
